4887892074fdb025efc632d96ff8b180601b2ed2
Fernando Soto <ferso@Fernandos-iMac.local>
2013-03-09 16:48:51 -0600

    Server fixed

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ var http  = require('http');
 var fs    = require('fs');
 var url   = require("url");
 var path  = require("path");
-var port  = 8000;
+var port  = 9000;
 var file   = 'index.html';
 http.createServer(function (req, res) {
+	 try{
 	 var uri = url.parse(req.url).pathname
 	, src    = path.join(process.cwd(), uri);	 
-	  src    = fs.statSync(src).isDirectory() ? src+'/'+file : src ;	
-	fs.readFile(src,'binary',function(err,data){
+	  src    = fs.statSync(src).isDirectory() ? src+'/'+file : '/'+src ;	
+	    console.log('/'+src);
+	 	fs.readFile(src,'binary',function(err,data){
 			res.writeHead(200);
 		    res.write(data,'binary'); 
 		    res.end();
 		});
+	 }catch(e){
+	 	console.error(e);
+	 }	
 }).listen(port);
 console.log('Server running at ' + port);
