20ba12ff524fff0635a298f2102b263b3b3f8651
Fernando Soto <ferso@Fernandos-iMac.local>
2013-03-08 22:23:31 -0600

    Server

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+/* By Fernando Soto @ferso
+contact: erickfernando@gmail.com
+-------------------------------------- */
 var http  = require('http');
 var fs    = require('fs');
 var url   = require("url");
 var path  = require("path");
 var port  = 8000;
-var file  = 'index.html';
-
+var file   = 'index.html';
 http.createServer(function (req, res) {
 	 var uri = url.parse(req.url).pathname
-	, filename = path.join(process.cwd(), uri);
-	if (fs.statSync(filename).isDirectory()) filename +='/'+file;	
-	fs.readFile(filename,'binary',function(err,data){
-		res.writeHead(200);
-	    res.write(data,'binary');
-	    res.end();
-	});
+	, src    = path.join(process.cwd(), uri);	 
+	  src    = fs.statSync(src).isDirectory() ? src+'/'+file : src ;	
+	fs.readFile(src,'binary',function(err,data){
+			res.writeHead(200);
+		    res.write(data,'binary'); 
+		    res.end();
+		});
 }).listen(port);
 console.log('Server running at ' + port);
